64d7ef52155ed9f05d9670c2228a7122ddd4424b
sengwoong <rje287573@gmail.com>
2023-02-28 15:07:06 +0900

    first commit

---
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.androidlab
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.Menu
+import android.widget.Toast
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedCallback
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}
